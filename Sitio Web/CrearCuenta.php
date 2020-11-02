d52aa9400b1b5cb04a49ab03e2d311612a897ed4
OzinoLautaro <taticaligaris@gmail.com>
2020-11-02 11:34:57 -0300

    Aparentemente, cURL viene con todos los Windows 10 XD

--- Sitio Web/CrearCuenta.php
+++ Sitio Web/CrearCuenta.php
@@ -11,15 +11,17 @@ $data = [
     'email' => $mail
 ];
 echo "entro data";
+$url = "http://127.0.0.1:4567/Usuarios";
 $curl = curl_init();
 echo "inicio curl";
-curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:4567/Usuarios")
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json'
+  ]);
 echo "todo correcto";
 $response = curl_exec($curl);
 curl_close($curl);
-echo $response.PHP_EOL;
 header("Location: RompePalabras.html");
 ?>
