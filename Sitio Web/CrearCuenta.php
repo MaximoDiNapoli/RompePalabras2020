2911ae8d373862c5b568c54d740fea3222b2a824
OzinoLautaro <taticaligaris@gmail.com>
2020-11-02 11:12:26 -0300

    Siguiendo con PHP

--- Sitio Web/CrearCuenta.php
+++ Sitio Web/CrearCuenta.php
@@ -1,18 +1,25 @@
 <?php
-$nombre=$_POST["user"];
-$mail=$_POST["email"];
-echo $mail;
-echo $nombre;
+session_start();
+$nombre=$_POST["username"];
+$mail=$_POST["mail"];
+if(empty($nombre) || empty($email)){
+    header("Location: CrearCuenta.html");
+    exit();
+    }
 $data = [
     'username' => $nombre,
     'email' => $mail
 ];
 echo "entro data";
-$url = "http://127.0.0.1:4567";
-$curl = curl_init($url);
+$curl = curl_init();
 echo "inicio curl";
+curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:4567/Usuarios")
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
 echo "todo correcto";
+$response = curl_exec($curl);
+curl_close($curl);
+echo $response.PHP_EOL;
+header("Location: RompePalabras.html");
 ?>
